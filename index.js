dc8ce97e369b022ed050ffded9c27e844280c07c
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2022-11-06 10:41:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+function handleclick() {
+var buttoninnerhtml = this.innerHTML;
+buttonAnimation(buttoninnerhtml);
+makeSound(buttoninnerhtml);
+}
+function makeSound(key){
+     
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();   
+            break;
+    
+            case "a":
+            var kickbass = new Audio("sounds/kick-bass.mp3");
+            kickbass.play();   
+            break;
+    
+            case "s":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();   
+                break;
+    
+            case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();   
+                break;
+                
+            case "j":
+                    var tom2 = new Audio("sounds/tom-2.mp3");
+                    tom2.play();   
+                    break;
+            
+            case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();   
+            break; 
+    
+            case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();   
+                break;
+        
+                    
+    
+        default:
+            break;
+
+
+
+    }
+
+
+
+}
+document.addEventListener("keydown", function(event){makeSound(event.key);
+    console.log(event);
+    buttonAnimation(event.key);
+})
+
+var numof_button =document.querySelectorAll(".drum").length;
+for( var i = 0; i< numof_button ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+} 
+function buttonAnimation(current){
+   var activeButton = document.querySelector("."+current);
+   activeButton.classList.add("pressed");
+   setTimeout(function(){
+    activeButton.classList.remove("pressed");
+   },100);
+}
+
+
